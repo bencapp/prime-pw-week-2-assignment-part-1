638ce0c503fc327cc101e020a1b89b062cd48092
Ben Capp <bentucker333@gmail.com>
2022-12-08 18:40:04 -0600

    Added conditionals

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -17,7 +17,7 @@ console.log(
   "My name is " + fullName + ", and I think " + luckyNumber + " is a winner!"
 );
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-let adventurous = "true";
+let adventurous = true;
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
 let food = "hot chocolate";
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
@@ -30,10 +30,16 @@ pets += 2;
 const allowedPets = 0;
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!",
 // if it's not true, console log "How about we stay home?"
-
+if (adventurous) {
+  console.log("Adventures are great!");
+} else {
+  console.log("How about we stay home?");
+}
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
-
+if (luckyNumber == 2 && adventurous) {
+  console.log("Roll the dice!");
+}
 // 15 - Write a conditional that console logs "I can have more pets!"
 //if the value of `pets` is less than the value of `allowedPets`,
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,

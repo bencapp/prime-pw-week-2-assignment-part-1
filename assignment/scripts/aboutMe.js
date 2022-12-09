84055fb5d17030b121f5c5cae0d76617b58c969d
Ben Capp <bentucker333@gmail.com>
2022-12-08 18:45:08 -0600

    Added allowed pets conditional

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -45,7 +45,13 @@ if (luckyNumber == 2 && adventurous) {
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
-
+if (pets < allowedPets) {
+  console.log("I can have more pets!");
+} else if (pets == 0) {
+  console.log("I have enough pets");
+} else {
+  console.log("Oh no, I have too many pets!");
+}
 // STRETCH GOALS:
 
 // 16 - Make a variable called `mostPets` and a conditional that

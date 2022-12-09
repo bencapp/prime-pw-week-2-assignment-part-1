35d1c2f0f7f4b1cb0e1c7c7a69544d8e459bd726
Ben Capp <bentucker333@gmail.com>
2022-12-08 18:37:47 -0600

    added pets, food, adventurous variables

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -17,17 +17,17 @@ console.log(
   "My name is " + fullName + ", and I think " + luckyNumber + " is a winner!"
 );
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-
+let adventurous = "true";
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
-
+let food = "hot chocolate";
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
-
+let pets = 0;
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-
+let friendsPets = 2;
 // 11 - Add two pets to your `pets` variable
-
+pets += 2;
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-
+const allowedPets = 0;
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!",
 // if it's not true, console log "How about we stay home?"
 
